4e1a692ecbac31f27db92e17d2458c30583bcf52
Christ Amron A. Luzon <11ict1.luzon@gmail.com>
2025-04-27 10:19:01 +0800

    Update Firebase initialization to use environment variables

--- server/index.js
+++ server/index.js
@@ -4,17 +4,23 @@ const admin = require('firebase-admin');
 const adminRoutes = require('./routes/admin');
 
 // Initialize Firebase Admin
-const serviceAccount = require('./config/serviceAccountKey.json');
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://appdev-86a96-default-rtdb.asia-southeast1.firebasedatabase.app"
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+  }),
+  databaseURL: process.env.FIREBASE_DATABASE_URL
 });
 
 const app = express();
 
 // Configure CORS
 app.use(cors({
-  origin: /^http:\/\/localhost:\d+$/, // Allow any localhost port
+  origin: [
+    /^http:\/\/localhost:\d+$/, // Allow any localhost port
+    /^https:\/\/.*\.netlify\.app$/ // Allow any Netlify domain
+  ],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
